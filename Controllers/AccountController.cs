77419521a56abcdceed390994a792796cf31698d
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-13 13:23:34 -0500

    Section 2 - Register View

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using IdentityManager.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            RegisterViewModel registerViewModel = new();
+            return View(registerViewModel);
+        }
+    }
+}
