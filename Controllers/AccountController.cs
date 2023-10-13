f0b04be3b364f3b1ce365b82d0e7d56025563edd
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-13 14:21:27 -0500

    Section 2 - Logout in Action

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -47,6 +47,16 @@ namespace IdentityManager.Controllers
             return View(model);
         }
 
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
         private void AddErrors(IdentityResult result)
         {
             foreach (var error in result.Errors)
