043010a743e4c8c759cc2259ee69d24ca2957362
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-13 12:37:16 -0500

    Section 1 - Configure DbContext in Program Class File

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace IdentityManager.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,14 @@
+using IdentityManager.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options=>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
