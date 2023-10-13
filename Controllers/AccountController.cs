5fefca8f622cb64edeb7e03e3d5599234b8b7225
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-13 13:53:25 -0500

    Section 2 - Register First User

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,14 +1,48 @@
-﻿using IdentityManager.Models.ViewModels;
+﻿using IdentityManager.Models;
+using IdentityManager.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManager.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Register()
         {
             RegisterViewModel registerViewModel = new();
             return View(registerViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
         }
     }
 }
