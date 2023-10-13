2e19ad5f9b75268d53f694de247ff43f8c2f1bee
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-13 14:35:10 -0500

    Section 2 - Login in Action

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -61,6 +61,27 @@ namespace IdentityManager.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe,
+                    lockoutOnFailure:false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+
 
         private void AddErrors(IdentityResult result)
         {
