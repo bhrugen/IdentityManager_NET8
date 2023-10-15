68e5f08a43ef7faf5fe321b10b7f156d320102be
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-15 13:04:51 -0500

    Section 4 - Setup Two Factor Auth on Account

--- Models/ViewModels/TwoFactorAuthenticationViewModel.cs
+++ Models/ViewModels/TwoFactorAuthenticationViewModel.cs
@@ -3,6 +3,6 @@
     public class TwoFactorAuthenticationViewModel
     {
         public string Code { get; set; }
-        public string Token { get; set; }
+        public string? Token { get; set; }
     }
 }
