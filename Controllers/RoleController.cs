27700e1f1ad8dc9a64b2117ddc1b7d6fa5d5b1b8
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-16 15:52:11 -0500

    Section 6 - View for creating and updating role

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,44 @@
+﻿using IdentityManager.Data;
+using IdentityManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+        public IActionResult Index()
+        {
+            var roles = _db.Roles.ToList();
+            return View(roles);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(string roleId)
+        {
+            if (String.IsNullOrEmpty(roleId))
+            {
+                //create
+                return View();
+            }
+            else
+            {
+                //update
+                var objFromDb = _db.Roles.FirstOrDefault(u => u.Id == roleId);
+                return View(objFromDb);
+            }
+        }
+    }
+}
