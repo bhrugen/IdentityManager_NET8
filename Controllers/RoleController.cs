b67f3d4259e346e1b8fb9de74c6436cb5fc2580b
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-16 19:59:17 -0500

    Section 6 - Check before deleting role

--- Controllers/RoleController.cs
+++ Controllers/RoleController.cs
@@ -75,9 +75,19 @@ namespace IdentityManager.Controllers
                 var objFromDb = _db.Roles.FirstOrDefault(u => u.Id == roleId);
             if (objFromDb != null)
             {
+
+                var userRolesForThisRole = _db.UserRoles.Where(u => u.RoleId == roleId).Count();
+                if (userRolesForThisRole > 0)
+                {
+                    TempData[SD.Error] = "Cannot delete this role, since there are users assigned to this role.";
+                    return RedirectToAction(nameof(Index));
+                }
+
                 var result = await _roleManager.DeleteAsync(objFromDb);
                 TempData[SD.Success] ="Role deleted successfully";
             }
+
+            TempData[SD.Error] = "Role not found.";
             return RedirectToAction(nameof(Index));
         }
 
