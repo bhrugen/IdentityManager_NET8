386fe7573e68ed7075b44b8a9e26273b1ec8b559
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-28 09:02:07 -0500

    Section 7 - Roles View Model

---
+++ Models/ViewModels/RolesViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace IdentityManager.Models.ViewModels
+{
+    public class RolesViewModel
+    {
+        public RolesViewModel()
+        {
+            RolesList = [];
+        }
+        public ApplicationUser User { get; set; }
+        public IEnumerable<RoleSelection> RolesList { get; set; }
+    }
+
+
+    public class RoleSelection
+    {
+        public string RoleName { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
