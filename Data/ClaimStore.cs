3acf64bd2fe6a7742a958b41eeb91f1db25a5b54
Bhrugen Patel <bhrugen1990@hotmail.com>
2023-10-28 12:20:29 -0500

    Section 8 - Claim Store

---
+++ Data/ClaimStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace IdentityManager.Data
+{
+    public static class ClaimStore
+    {
+        public static List<Claim> claimsList =
+            [
+                new Claim("Create", "Create"),
+                new Claim("Edit", "Edit"),
+                new Claim("Delete", "Delete")
+            ];
+    }
+}
